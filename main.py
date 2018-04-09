d9e785619e2da03983487c08afaaa2e4a55242dd
yuesj <479100885@qq.com>
2018-04-09 23:55:25 +0800

    爬虫伯乐网

---
+++ main.py
@@ -0,0 +1,16 @@
+#! /usr/local/bin python3.6
+"""
+@Time    : 2018/4/9 21:47
+@Author  : ysj
+@Site    : 
+@File    : main.py
+@Software: PyCharm
+"""
+from scrapy.cmdline import execute
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+execute(['scrapy', 'crawl', 'jobbole'])
